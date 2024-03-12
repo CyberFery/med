86c2c50adbba5254bf974a0a9b883c873d00ddd7
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-03-12 11:36:39 -0400

    Refactor modules

---
+++ backend/modifications-archive-service/src/main/java/uqam/team17/modificationsarchiveservice/ModificationsArchiveServiceApplication.java
@@ -0,0 +1,13 @@
+package uqam.team17.modificationsarchiveservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModificationsArchiveServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModificationsArchiveServiceApplication.class, args);
+    }
+
+}
---
+++ backend/ramq-service/src/main/java/uqam/team17/ramqservice/RamqServiceApplication.java
@@ -0,0 +1,13 @@
+package uqam.team17.ramqservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamqServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RamqServiceApplication.class, args);
+    }
+
+}
