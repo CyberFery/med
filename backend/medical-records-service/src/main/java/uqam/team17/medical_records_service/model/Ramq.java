fdb0deacffeab10277c28e29035a588a261e3e8b
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-03-12 11:36:25 -0400

    Update packages diagrams images

--- backend/medical-records-service/src/main/java/uqam/team17/medical_records_service/model/Ramq.java
+++ backend/medical-records-service/src/main/java/uqam/team17/medical_records_service/model/Ramq.java
@@ -1,5 +1,7 @@
 package uqam.team17.medical_records_service.model;
 
+import uqam.team17.medical_records_service.model.medical_record.MedicalRecord;
+
 public class Ramq {
     void registerPerson() {
     }
