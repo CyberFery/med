a9d28acbe70c3f55c3c5e6d46df120675f7fc454
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-03-27 00:32:59 -0400

    Add Doctor entity class

---
+++ backend/ramq-service/src/main/java/uqam/team17/ramqservice/entity/Doctor.java
@@ -0,0 +1,59 @@
+package uqam.team17.ramqservice.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String specialization;
+
+    public Doctor() {
+        // default constructor
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", specialization='" + specialization + '\'' +
+                '}';
+    }
+}
