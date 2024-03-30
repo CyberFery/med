a86145c9de8adc739fdb65ebbbd4a66d0ece94c4
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-03-30 13:25:59 -0400

    Add components to app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,15 @@
 import AppBar from "@/components/AppBar.vue";
 import Footer from "@/components/Footer.vue";
 import Tabs from "@/components/Tabs.vue";
+
 </script>
 
 <template>
   <v-app>
     <AppBar/>
     <v-main>
+      <Tabs/>
+      <router-view/>
     </v-main>
     <Footer/>
   </v-app>
