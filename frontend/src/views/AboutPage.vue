306db0f9f331ea1b1b37b68560008f76b54a1603
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-03-30 13:22:12 -0400

    Add basic view files

---
+++ frontend/src/views/AboutPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-container>
+    <h1 class="text-center">About Page</h1>
+    <!-- Add content for the home page -->
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/NotFoundPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-container>
+    <h1 class="text-center">Not Found Page</h1>
+    <!-- Add content for the home page -->
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
