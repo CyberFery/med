24deaf5bbcf26de3bc19eb776a52f74dc663617a
Lindsay Blemur <blemur.lindsay@courrier.uqam.ca>
2024-03-31 19:05:01 -0400

    Update adds the controller and endpoints

--- backend/modifications-archive-service/src/main/java/uqam/team17/modificationsarchiveservice/controller/ModificationArchiveController.java
+++ backend/modifications-archive-service/src/main/java/uqam/team17/modificationsarchiveservice/controller/ModificationArchiveController.java
@@ -1,27 +1,47 @@
-package uqam.team17.modificationsarchiveservice;
+package uqam.team17.modificationsarchiveservice.controller;
 
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-import uqam.team17.modificationsarchiveservice.service.*;
+import uqam.team17.modificationsarchiveservice.service.ModificationService;
+import uqam.team17.modificationsarchiveservice.model.Modification;
 import uqam.team17.modificationsarchiveservice.model.ModificationRequest;
+import uqam.team17.modificationsarchiveservice.model.ModificationIdRequest;
+
+
 
 @RestController
 public class ModificationArchiveController{
-    public static final String ROOT_PATH = "/modifications";
+    //public static final String ROOT_PATH = "/modifications";
 
-    private ModificationsArchiveService modificationsService;
+    private final ModificationService modificationService;
 
-    public ModificationArchiveController (ModificationsArchiveService modificationsArchiveService){
-        this.modificationsService = modificationsArchiveService;
+    public ModificationArchiveController (ModificationService modification){
+        this.modificationService = modification;
     }
 
-   @PostMapping(ROOT_PATH)
-    public void createModification(@RequestBody ModificationRequest){
+    @GetMapping("/modifications")
+    public Modification getModification(@RequestBody ModificationIdRequest idRequest){
+        Long modificationId = idRequest.getModificationId();
+        return modificationService.getModification(modificationId);
+    }
+
+
+   @PostMapping("/modifications")
+    public ResponseEntity<?> createModification(@RequestBody Modification modification){
+       if (modification == null || modification.getContent() == null) {
+           return ResponseEntity.badRequest().body("Invalid Modification Format!");
+       }
+
+       Modification mod = modificationService.saveModification(modification);
+
+       return ResponseEntity.ok(mod);
 
    }
+
+
 
 
 
