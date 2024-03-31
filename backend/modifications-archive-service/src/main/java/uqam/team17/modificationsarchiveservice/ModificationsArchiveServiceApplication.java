2acb750d65668795ae3ac69c95115caa162ef792
Lindsay Blemur <blemur.lindsay@courrier.uqam.ca>
2024-03-31 19:06:11 -0400

    Update fixes the application

--- backend/modifications-archive-service/src/main/java/uqam/team17/modificationsarchiveservice/ModificationsArchiveServiceApplication.java
+++ backend/modifications-archive-service/src/main/java/uqam/team17/modificationsarchiveservice/ModificationsArchiveServiceApplication.java
@@ -3,6 +3,7 @@ package uqam.team17.modificationsarchiveservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ModificationsArchiveServiceApplication {
 
