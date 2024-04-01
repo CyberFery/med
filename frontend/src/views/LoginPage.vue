cae4b7f68714c3cc58aec0407ab3f42e904642c0
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-04-01 16:23:28 -0400

    Implement form validation for user login in LoginPage view

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -1,14 +1,69 @@
 <script setup lang="ts">
+import {useField, useForm} from 'vee-validate'
 
+const {handleSubmit, handleReset} = useForm({
+  validationSchema: {
+    userName(value: any) {
+      if (value?.length >= 2 && value?.length <= 20) return true
+
+      return 'User name needs to be at least 2 and maximum 20 characters.'
+    },
+
+    password(value: any) {
+      if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/i.test(value)) return true
+
+      return 'Password must meet the following criteria: at least one uppercase letter, at least one lowercase letter, at least one digit, at least one special character (@$!%*?&), and a minimum length of 8 characters.'
+    },
+  },
+})
+
+const userName = useField('userName')
+const password = useField('password')
+
+const submit = handleSubmit(values => {
+  alert(JSON.stringify(values, null, 2))
+})
 </script>
 
 <template>
-  <v-container>
-    <h1 class="text-center">Login Page</h1>
-    <!-- Add content for the home page -->
-  </v-container>
+  <h1 class="text-center text-h2 mt-6 mb-4">User Secure Login</h1>
+
+  <v-row align="center" justify="center">
+    <v-col cols="12" sm="8" md="6">
+      <form class="text-center" @submit.prevent="submit">
+        <v-text-field
+          class="my-6"
+          v-model="userName.value.value"
+          :counter="20"
+          :error-messages="userName.errorMessage.value"
+          label="User Name"
+        />
+
+        <v-text-field
+          class="mt-6"
+          v-model="password.value.value"
+          :error-messages="password.errorMessage.value"
+          label="Password"
+          type="password"
+        />
+
+        <v-container>
+          <v-btn
+            class="bg-green-accent-1 me-4"
+            type="submit"
+          >
+            Submit
+          </v-btn>
+
+          <v-btn class="bg-red-accent-1" @click="handleReset">
+            Clear
+          </v-btn>
+        </v-container>
+
+      </form>
+    </v-col>
+  </v-row>
 </template>
 
 <style scoped>
-
 </style>
