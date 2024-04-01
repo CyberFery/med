d31a53706bb1ccc69aa86d44a3e7447bb61053e9
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-04-01 16:17:37 -0400

    Add dynamic menu items to AppBar component

--- frontend/src/components/AppBar.vue
+++ frontend/src/components/AppBar.vue
@@ -1,10 +1,55 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const items = ref([
+  {
+    pageName: "Home",
+    icon: "mdi-home",
+    path: "/"
+  },
+  {
+    pageName: "Login",
+    icon: "mdi-login",
+    path: "/login"
+  },
+  {
+    pageName: "About",
+    icon: "mdi-information",
+    path: "/about"
+  }
+]);
 </script>
 
 <template>
-  <v-app-bar class="bg-grey-darken-3" :elevation="2">
-    <v-app-bar-title>Doctor - Centralized Medical Records</v-app-bar-title>
-  </v-app-bar>
+  <v-toolbar class="bg-grey-darken-3" elevation="8">
+    <v-toolbar-title>Doctor - Centralized Medical Records</v-toolbar-title>
+
+    <v-spacer/>
+
+    <v-btn>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+
+    <template v-slot:extension>
+      <v-tabs
+        center-active
+        align-tabs="center"
+      >
+        <v-tab
+          v-for="item in items"
+          :key="item.path"
+          :to="item.path"
+        >
+          <v-icon>{{ item.icon }}</v-icon>
+          {{ item.pageName }}
+        </v-tab>
+      </v-tabs>
+    </template>
+  </v-toolbar>
 </template>
 
 <style scoped>
