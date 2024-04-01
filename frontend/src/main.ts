952b11569ac9a2a4f3930bb4b49951bd5fa48414
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-04-01 16:25:30 -0400

    Refactor route definitions in main.ts for consistency and add authentication check middleware

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -12,19 +12,36 @@ import AboutPage from '@/views/AboutPage.vue'
 import {createApp} from 'vue'
 
 // vue-router
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 // Create the router instance and define your routes
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: HomePage },
-    { path: '/login', component: LoginPage },
-    { path: '/not-found', component: NotFoundPage },
-    { path: '/about', component: AboutPage }
+    {path: '/', component: HomePage},
+    {path: '/login', component: LoginPage},
+    {path: '/not-found', component: NotFoundPage},
+    {path: '/about', component: AboutPage}
     // TODO Add more routes as needed
+    // example with authentication required :
+    // { path: '/patient/1', component: Patient, meta: { requiresAuth: true } }
   ]
 })
+
+// Function to check if the user is authenticated
+const checkAuth = () => {
+  const token = localStorage.getItem('token');
+  return !!token; // Check if token exists
+};
+
+// Check authentication on every route change
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !checkAuth()) {
+    next('/login'); // Redirect to log in if route requires authentication and user is not authenticated
+  } else {
+    next();
+  }
+});
 
 // Create the app instance and register plugins
 const app = createApp(App)
