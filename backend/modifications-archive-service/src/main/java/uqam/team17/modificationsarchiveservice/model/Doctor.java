2991d095a3a3558a0e40a4ad61c83275e6b1ad09
Lindsay Blemur <blemur.lindsay@courrier.uqam.ca>
2024-04-03 22:39:07 -0400

    Update removed the doctorId in the object Doctor

--- backend/modifications-archive-service/src/main/java/uqam/team17/modificationsarchiveservice/model/Doctor.java
+++ backend/modifications-archive-service/src/main/java/uqam/team17/modificationsarchiveservice/model/Doctor.java
@@ -1,15 +1,7 @@
 package uqam.team17.modificationsarchiveservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
-@Entity
 public class Doctor {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long doctorId;
     private String firstName;
     private String lastName;
     private String specialization;
@@ -42,18 +34,9 @@ public class Doctor {
         this.specialization = specialization;
     }
 
-    public Long getDoctorId() {
-        return doctorId;
-    }
-
-    public void setDoctorId(Long doctorId) {
-        this.doctorId = doctorId;
-    }
-
     @Override
     public String toString() {
         return "Doctor{" +
-                "doctorId=" + doctorId +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", specialization='" + specialization + '\'' +
