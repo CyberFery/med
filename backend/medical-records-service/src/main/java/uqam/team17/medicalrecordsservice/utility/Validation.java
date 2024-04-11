04ff097bd544b2ff76ab1bac0ca1e381fca4b859
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-04-11 08:40:15 -0400

    Refactor validation.java
    - Added private default constructor.

--- backend/medical-records-service/src/main/java/uqam/team17/medicalrecordsservice/utility/Validation.java
+++ backend/medical-records-service/src/main/java/uqam/team17/medicalrecordsservice/utility/Validation.java
@@ -4,11 +4,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validation {
+    private Validation() {
+        // default constructor
+    }
     public static boolean validHealthInsuranceNumber(String healthInsuranceNumber) {
         String regex = "^[a-zA-Z]{4}\\d{8,10}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(healthInsuranceNumber.trim().toUpperCase());
 
-        return matcher.matches();
+        return !matcher.matches();
     }
 }
